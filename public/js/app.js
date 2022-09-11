64e9c8ec19c4f8ed69e6ad3d76bc1876b3578c80
sandesh-personal <112658218+sandesh-personal@users.noreply.github.com>
2022-09-11 16:44:01 +0545

    vm update

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', e => {
     let url = e.target.url.value; // get value of input
     let domain = url.split('/')[2]; // get domain
 
-    if(domain === 'www.tiktok.com', 'http://vt.tiktok.com' || domain === 'vm.tiktok.com'){
+    if(domain === 'www.tiktok.com', 'http://vt.tiktok.com', 'https://vm.tiktok.com' || domain === 'vm.tiktok.com'){
         getUrl(url); // get data video
     }else{
         content.innerHTML = '<h3 class="messageError">Error, The url is not a tiktok link!</h3>'
